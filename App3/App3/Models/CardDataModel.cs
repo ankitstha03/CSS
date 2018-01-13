61c964a0913481fca151debae5637d2ac4bbb718
bikram101 <bekrumed1@gmail.com>
2018-01-13 07:53:58 +0545

    Data model for card view added

---
+++ App3/App3/Models/CardDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3.Models
+{
+    public class CardDataModel
+    {
+        public string HeadTitle { get; set; }
+        public string HeadLines { get; set; }
+        public string HeadLinesDesc { get; set; }
+        public string ProfileImage { get; set; }
+    }
+}
